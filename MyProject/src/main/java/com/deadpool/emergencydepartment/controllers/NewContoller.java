dcec88f681b48faa7ddad479f628ff3984640ca3
yusuf33NN <m-muradov@list.ru>
2021-05-01 16:15:33 +0300

    do not know

---
+++ MyProject/src/main/java/com/deadpool/emergencydepartment/controllers/NewContoller.java
@@ -0,0 +1,11 @@
+package com.deadpool.emergencydepartment.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NewContoller {
+
+    public void whatTF() {
+        System.out.println("WTF?");
+    }
+}
